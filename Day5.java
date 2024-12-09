265b0a0f380213fe36decb068c047ac1553685b8
TerryT1 <144486954+TerryT1@users.noreply.github.com>
2024-12-09 14:06:11 -0500

    Add files via upload

--- Day5.java
+++ Day5.java
@@ -2,17 +2,26 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Day5 {
     public static void main(String[] args) {
 
         ArrayList<String> fileData = getFileData("src/Day1Input.txt");
-        System.out.println(fileData);
+        ArrayList<String> allMatches = new ArrayList<String>();
+
         // you now have an ArrayList of Strings for each number in the file
         // do Advent 2020 day 1!
 
         String searchString = String.valueOf(fileData);
-        String regex = "\\(([1-9][0-9]{0,1})|([1-9][0-9]{0,1})\\";
+        String regex = "\\([1-9][0-9]{0,1})|([1-9][0-9]{0,1}\\";
+        Matcher m = Pattern.compile(regex).matcher(searchString);
+        while (m.find()) {
+            allMatches.add(m.group());
+        }
+        System.out.println(allMatches);
+
     }
 
     public static ArrayList<String> getFileData(String fileName) {
