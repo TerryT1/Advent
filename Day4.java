074db55598f54a8b49ac2df69f6995739ec28146
TerryT1 <144486954+TerryT1@users.noreply.github.com>
2024-12-11 13:41:03 -0500

    Update Day4.java

--- Day4.java
+++ Day4.java
@@ -2,31 +2,58 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Day4 {
     public static void main(String[] args) {
+        ArrayList<String> fileData = getFileData("src/Day4Input.txt");
 
-        ArrayList<String> fileData = getFileData("Day3Input.txt");
-        ArrayList<String> allMatches = new ArrayList<String>();
+        int sum = 0;
+        int rows = fileData.size();
+        int columns = fileData.get(0).length();
+        String[][] grid = new String[rows][columns];
 
-        // you now have an ArrayList of Strings for each number in the file
-        // do Advent 2020 day 1!
-
-        String searchString = String.valueOf(fileData);
-        String regex = "(\\d+\\|\\d+)";
-
-        Matcher m = Pattern.compile(regex).matcher(searchString);
-        while (m.find()) {
-            allMatches.add(m.group());
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < columns; c++) {
+                grid[r][c] = fileData.get(r).substring(c, c + 1);
+            }
         }
 
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < columns; c++) {
+                if (c + 3 < columns && (grid[r][c] + grid[r][c + 1] + grid[r][c + 2] + grid[r][c + 3]).equals("XMAS")) {
+                    sum++;
+                }
+                if (r + 3 < rows && (grid[r][c] + grid[r + 1][c] + grid[r + 2][c] + grid[r + 3][c]).equals("XMAS")) {
+                    sum++;
+                }
+                if (r + 3 < rows && c + 3 < columns && (grid[r][c] + grid[r + 1][c + 1] + grid[r + 2][c + 2] + grid[r + 3][c + 3]).equals("XMAS")) {
+                    sum++;
+                }
+                if (r + 3 < rows && c - 3 >= 0 && (grid[r][c] + grid[r + 1][c - 1] + grid[r + 2][c - 2] + grid[r + 3][c - 3]).equals("XMAS")) {
+                    sum++;
+                }
+                if (c - 3 >= 0 && (grid[r][c] + grid[r][c - 1] + grid[r][c - 2] + grid[r][c - 3]).equals("XMAS")) {
+                    sum++;
+                }
+                if (r - 3 >= 0 && (grid[r][c] + grid[r - 1][c] + grid[r - 2][c] + grid[r - 3][c]).equals("XMAS")) {
+                    sum++;
+                }
+                if (r - 3 >= 0 && c - 3 >= 0 && (grid[r][c] + grid[r - 1][c - 1] + grid[r - 2][c - 2] + grid[r - 3][c - 3]).equals("XMAS")) {
+                    sum++;
+                }
+                if (r - 3 >= 0 && c + 3 < columns && (grid[r][c] + grid[r - 1][c + 1] + grid[r - 2][c + 2] + grid[r - 3][c + 3]).equals("XMAS")) {
+                    sum++;
+                }
+            }
+        }
 
-
-        System.out.println(allMatches);
-
+        System.out.println(sum);
     }
+
+
+
+    // "grid" represents a 2D array of Strings built from the input file
+
 
     public static ArrayList<String> getFileData(String fileName) {
         ArrayList<String> fileData = new ArrayList<String>();
